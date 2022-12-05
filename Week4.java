7fb685ccec143b133f94e8aee0751a800444285f
JD4097 <jmabhatt@yahoo.co.in>
2022-12-05 23:14:10 +0000

    week4to5

---
+++ Week4.java
@@ -0,0 +1,20 @@
+package org.Week4to5;
+
+public class Week4 {
+    public static void main( String[] args )
+    {
+        System.out.println( "Topic Covered in Week 4" );
+        System.out.println( "******************");
+        System.out.println( "Static vs Dynamic Testing");
+        System.out.println( "Exploratory Testing");
+        System.out.println( "Ad-Hoc Testing");
+        System.out.println( "Software Development Models");
+        System.out.println( "     >Waterfall");
+        System.out.println( "     >Iterative");
+        System.out.println( "     >Verification and Validation");
+        System.out.println( "     >V-Model");
+        System.out.println( "     >Agile.");
+        System.out.println( "     >Definition of Ready/Done");
+        System.out.println( "*************************************");
+    }
+}
---
+++ Week5.java
@@ -0,0 +1,15 @@
+    package org.Week4to5;
+
+public class Week5 {
+        public static void main( String[] args )
+        {
+            System.out.println( "Topic Covered in Week 5" );
+            System.out.println( "******************");
+            System.out.println( "What is TDD?");
+            System.out.println( "What is BDD?");
+            System.out.println( "     >BDD keywords");
+            System.out.println( "     >How to write test cases with BDD?");
+            System.out.println( "Maintenance");
+            System.out.println( "*************************************");
+        }
+}
